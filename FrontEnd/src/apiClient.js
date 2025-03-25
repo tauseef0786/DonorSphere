afc65963daf7c2ae54a8d3bb950a25429faf9bd2
tauseed0786 <tauseefm948@gmail.com>
2025-03-25 14:44:29 +0530

    api

--- FrontEnd/src/apiClient.js
+++ FrontEnd/src/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = "http://localhost:3030/api";
+const baseUrl = "https://donorsphere.onrender.com/api";
 
 export const apiClient = axios.create({
     baseURL: baseUrl,
